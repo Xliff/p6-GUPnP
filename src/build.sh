8dcf478c55f747999dfc5e1cc0783e66cbaeac42
Clifton Wood <clifton.wood@gmail.com>
2021-01-28 02:29:25 -0500

    - Updated build script for gupnp-helper.so

--- src/build.sh
+++ src/build.sh
@@ -1,1 +1,1 @@
-gcc `pkg-config --cflags gupnp-1.2` gupnp-helper.c `pkg-config --libs gupnp-1.2` -fPIC -shared -o ../resources/lib/libgupnp-helper.so
+gcc `pkg-config --cflags gupnp-av-1.0` `pkg-config --cflags gupnp-1.2` gupnp-helper.c `pkg-config --libs gupnp-av-1.0` `pkg-config gupnp-1.2` -fPIC -shared -o ../resources/lib/libgupnp-helper.so
